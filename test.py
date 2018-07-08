6c6d943934bfc2ee0efe2589e6301db1ea67abd1
ama0322 <ma.aaron.ma@gmail.com>
2018-07-08 10:03:48 -0700

    Updates test.py to use the new testing_execute() function(actual encryption + decryption moved to miscellaneous. Only thing left in test.py is to gather the parameters for encryption and decryption.)

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-import cryptography_runner # for decryption_set
 import time # for timing
 import datetime # for labelling the date that files are created
 import miscellaneous
@@ -15,7 +14,9 @@ plaintext_source = "Library/Eleonora.txt"
 char_set_size = miscellaneous.char_set_to_char_set_size.get("unicode_plane0")
 key = "This is a key for testing"
 
-char_scheme_size = miscellaneous.char_set_to_char_set_size.get("base4096")
+
+
+encoding_scheme = "base64"
 
 
 
@@ -23,62 +24,96 @@ char_scheme_size = miscellaneous.char_set_to_char_set_size.get("base4096")
 # This function enters the testing mode where the user can enter commands to see check ciphers and see stats
 def testing_mode():
 
-        # Forever while loop to take in user commands and execute them
-        while True:
+    # Forever while loop to take in user commands and execute them
+    while True:
 
-                # Get the decryption type and the encryption type (same name as decyption but w/o "_nokey")
-                decryption = _parse_user_input()
-                encryption = decryption[0: decryption.find("_nokey")]
+        # Get information necessary for encrypting and decryption
+        encryption, decryption, plaintext, output_location, encryption_key, alphabet = _get_testing_info()
 
-                # Obtain the plaintext
-                my_file = open(plaintext_source, "r", encoding="utf-8")
-                plaintext = my_file.read()
-                my_file.close()
-
-                # Create the output location for this run
-                now = datetime.datetime.now()
-                output_location = "Files_Logs/" + decryption + "_" + now.strftime("%Y-%m-%d_h%Hm%Ms%S")
-
-
-                # Conduct test for symmetric ciphers
-                if decryption in miscellaneous.symmetric_ciphers:
-
-                    # Adjust the key for the encryption used (1st char for single character encryption like rotation)
-                    if miscellaneous.encryption_key_type.get(encryption) == 1:
-                        global key;
-                        key = key[0]
-
-                    # Encrypt the plaintext.
-                    start_time = time.time()
-                    exec("from Encryption import " + encryption)
-                    ciphertext = eval(encryption + ".encrypt(plaintext, key, char_set_size)")
-                    encryption_time = time.time() - start_time
-
-                    # Figure out if the decryption method needs a key ("_nokey" doesn't need one)
-                    needs_key = "_nokey" not in decryption
-
-                    # Decrypt the plaintext. Call testing execute
-                    exec("from Decryption import " + decryption)
-                    exec(decryption + ".testing_execute(ciphertext, output_location, plaintext, key, "
-                                      "                 char_set_size, encryption_time)")
+        # Pass this info to decryption's testing_execute
+        exec("import Decryption." + decryption)
+        exec("Decryption." + decryption
+             + ".testing_execute(encryption, decryption, plaintext, encryption_key, alphabet, output_location)")
 
 
 
 
-                # Conduct test for asymmetric ciphers
-                elif decryption in miscellaneous.asymmetric_ciphers:
 
-                    # Encrypt the plaintext, and get the generated ciphers
-                    start_time = time.time()
-                    exec("from Encryption import " + encryption)
-                    ciphertext, public_key, private_key = eval(encryption + ".encrypt(plaintext, \"\", "
-                                                                          + "char_scheme_size)")
-                    encryption_time = time.time() - start_time
 
-                    # Decrypt the plaintext using the private key. Call testing execute
-                    exec("from Decryption import " + decryption)
-                    exec(decryption + ".testing_execute(ciphertext, output_location, plaintext, public_key, "
-                                      + "private_key, char_scheme_size, encryption_time)")
+
+
+
+
+# Obtain information necessary to conduct encryption and decryption
+def _get_testing_info():
+
+    # Values to return
+    encryption = ""
+    decryption = ""
+    plaintext = ""
+    output_location = ""
+    encryption_key = key                                    # Modify key as needed for the chosen cipher
+    alphabet = ""                                           # num for char sets, name for encoding scheme
+
+    # Get the decryption type and the encryption type (same name as decyption but w/o "_nokey")
+    decryption = _parse_user_input()
+    encryption = decryption if decryption.find("_nokey") == -1 else decryption[0: -6]
+
+    # Obtain the plaintext
+    my_file = open(plaintext_source, "r", encoding="utf-8")
+    plaintext = my_file.read()
+    my_file.close()
+
+    # Create the output location for this run
+    now = datetime.datetime.now()
+    output_location = "Files_Logs/" + decryption + "_" + now.strftime("%Y-%m-%d_h%Hm%Ms%S")
+
+
+    # Figure out the correct key to use (read key_size)
+    # "zero characters"               is an encrypting cipher that doesn't need a key input
+    # "calculated characters"         is a decrypting cipher that finds the key automatically
+    # "single character"              is a symmetric encrypting/decrypting cipher that uses a single user-entered
+    #                                 character
+    # "multiple characters"           is an symmetric encrypting/decrypting cipher that uses user-entered string
+    # "multiple generated characters" is a symmetric encrypting/decrypting cipher that uses randomly generated chars
+    exec("import Decryption." + decryption)                               # Import module to read cipher properties
+    key_size =    eval("Decryption." + decryption + ".key_size")          # The size of the key used (see comment above)
+    alphabet =    eval("Decryption." + decryption + ".alphabet")          # Alphabet used
+    cipher_type = eval("Decryption." + decryption + ".cipher_type")       # Symmetric or asymmetric
+
+
+    if cipher_type == "symmetric":
+        if key_size ==     "zero characters":
+            encryption_key = ""
+
+        elif key_size.find("calculated characters") != -1:                # If calculated, read on
+            if key_size[22:] == "(single character)": encryption_key = key[0]
+            elif key_size[22:] == "(multiple characters)": encryption_key = key
+            elif key_size[22:] == "(multiple generated characters)": encryption_key = ""
+
+        elif key_size ==   "single character":
+            encryption_key = key[0]
+
+        elif key_size ==   "multiple characters":
+            encryption_key = key
+
+        elif key_size ==   "multiple generated characters":               # Key is generated for us
+            encryption_key = ""
+
+
+    elif cipher_type == "asymmetric":                                     # Key is generated for us
+        encryption_key = ""
+
+
+    # Figure out the alphabet to use
+    if alphabet == miscellaneous.char_encoding_schemes:                   # If encoding scheme, just return name
+        alphabet = encoding_scheme
+    elif alphabet == miscellaneous.char_sets:                             # If char set, return the size of the set
+        alphabet = char_set_size
+
+
+    return encryption, decryption, plaintext, output_location, encryption_key, alphabet
+
 
 
 
@@ -125,6 +160,7 @@ def _parse_user_input():
                 statement = input("Invalid command! Enter a testing mode command: ")
 
         return statement
+
 
 
 
