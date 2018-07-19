22c48eaf659f27ffd5571f2d007e4725accb843f
SysAdmin <ama0322>
2018-07-18 19:21:29 -0700

    Made progress on encrypt/decrypt command.

--- Cryptography/cryptography_runner.py
+++ Cryptography/cryptography_runner.py
@@ -54,9 +54,9 @@ def main():
 def usage():
     print("ENCRYPTION/DECRYPTION TYPES AVAILABLE: ")
     print("Available for both encryption and decryption: ", end = "")
-    print(*(misc.encryption_set & misc.decryption_set), sep=", ")
+    print(*(misc.ENCRYPTION_SET & misc.DECRYPTION_SET), sep=", ")
     print("Available for decryption only: ", end = "")
-    print(*(misc.decryption_set - misc.encryption_set), sep=", ")
+    print(*(misc.DECRYPTION_SET - misc.ENCRYPTION_SET), sep=", ")
     print()
 
 
@@ -69,6 +69,7 @@ def parse_user_input():
         decrypt (cipher) (ciphertext_file_path) <output_file_path>
         test    [options] <cipher>
         help
+        exit
         database TODO
 
     * File paths with spaces in the name must be escaped with "\ ".
@@ -107,69 +108,197 @@ def parse_user_input():
         # Handle command: help
         if command == "help":
 
-            # Execute help. So print out helpful information and return a prompt.
-            def handle_help():
-                """
-                Prints out testing information for the user to use, returns the prompt to use
-
-                :return: (string) The prompt given to the user
-                """
-                usage()
-                return "Enter statement: "
-            statement = input(handle_help())                            # Obtain user input for next iteration
+            usage()                                                     # Print helpful info
+            statement = input("Enter statement: ")                      # Obtain user input for next iteration
             continue                                                    # Jump to next iteration
 
 
         # Handle command: test <cipher>
         if command == "test":
 
-            # Execute test command. Parse any optional arguments and enter testing mode. When exiting testing mode, ask
-            # the user to enter another statement
-            def handle_test(statement):
-                """
-                Enters the asked for testing mode. When testing is done, asks the user for another statement.
+            prompt = ""                                                 # The prompt to return
+            statement = statement.split(" ")                            # Split statement into words separated by spaces
 
-                :return: (string) The prompt given to the user
-                """
+            # There should only be two arguments total (the command + option) or (the command + optional_arg). If
+            # there are more arguments than two, then return error
+            if len(statement) > 2:
+                extra_args = " ".join(statement)                        # List to string
+                extra_args = extra_args[extra_args.find(" ",            # extra args is after second space
+                                 extra_args.find(" ") + 1) + 1:]
+                prompt = "Extra argument (" + extra_args + ") given! Enter another statement: "
 
-                prompt = ""                                                        # The prompt to return
+            # If no optional arguments or flags provided, enter testing mode with no cipher provided
+            if len(statement) == 1:
+                test.manual_testing("")
+                prompt = "Manual testing done! Enter another statement: "
 
-                # Split statement into words separated by spaces
-                statement = statement.split(" ")
+            # Read for the "-a" flag for automated testing
+            if statement[1] == "-a":
+                test.automated_testing()
+                prompt = "Automated testing done! Enter another statement: "
 
-                # There should only be two arguments total (the command + option) or (the command + optional_arg). If
-                # there are more arguments than two, then return error
-                if len(statement) > 2:
-                    extra_args = " ".join(statement)                                                  # List to string
-                    extra_args = extra_args[extra_args.find(" ", extra_args.find(" ") + 1) + 1:]      # After 2nd space
-                    return "Extra argument (" + extra_args + ") given! Enter another statement: "
+            # If a Decryption cipher is provided, enter manual testing and run the test on that cipher
+            else:
+                test.manual_testing(statement[1])
+                prompt = "Manual testing done! Enter another statement: "
 
-                # If no optional arguments or flags provided, enter testing mode with no cipher provided
-                if len(statement) == 1:
-                    test.manual_testing("")
-                    return "Manual testing done! Enter another statement: "
-
-                # Read for the "-a" flag for automated testing
-                if statement[1] == "-a":
-                    test.automated_testing()
-                    return "Automated testing done! Enter another statement: "
-
-                # If a Decryption cipher is provided, enter manual testing and run the test on that cipher
-                else:
-                    test.manual_testing(statement[1])
-                    return "Manual testing done! Enter another statement: "
-            statement = input(handle_test(statement))                   # Obtain user input for next iteration
+            statement = input(prompt)                                   # Obtain user input for next iteration
             continue                                                    # Jump to the next iteration
 
+        # Handle command: exit
+        if command == "exit":
+            print("Exiting program...")
+            exit(0)
 
-        # Handle command: encrypt (cipher) (plaintext_file_path*) <output_file_path>
-        if command == "encrypt":
-            pass
+
+        # Handle command: encrypt or decrypt (cipher) (plaintext_file_path*) <output_file_path>
+        if command == "encrypt" or command == "decrypt":
 
 
-        # Handle command: decrypt (cipher) (ciphertext_file_path*) <output_file_path>
-        if command == "decrypt":
-            pass
+            # Set encrypt_mode based on command encrypt/decrypt
+            if command == "decrypt": encrypt_mode = False
+
+
+            # Function to split statement into list separated by spaces. (file_paths may have spaces escaped with "\ ")
+            def statement_to_list(statement):
+                """
+                This splits a string at every space not escaped. Elements are stored into a list
+
+                :param   (string) the string to split
+                :return: (list) the split string
+                """
+
+                list_to_return = []                            # Construct the list here
+
+
+                # Loop through the statement, and split when space is occurred. Ignore escaped's spaces ("\ ")
+                while statement != "":
+
+                    unescaped_space_index = -1
+
+                    # Loop until an unescaped space is found
+                    while True:
+
+                        unescaped_space_index = statement.find(" ", unescaped_space_index + 1)    # Find space index
+
+                        if unescaped_space_index == -1:                                           # If no more spaces
+                            list_to_return.append(statement)
+                            statement = ""
+                            break
+
+                        if statement[unescaped_space_index - 1] == "\\":                          # If escaped, continue
+                            continue
+                        else:                                                                     # Else, space found
+                            break
+
+                    # If statement is empty, just break
+                    if statement == "":
+                        break
+
+                    # Replace all escaped spaces up to the unescaped_space_index with just a space
+                    list_element = statement[0:unescaped_space_index]
+                    list_element = list_element.replace("\ ", " ")
+
+                    # Save the segment up to the space and cut out from var statement
+                    list_to_return.append(list_element)
+                    statement = statement[unescaped_space_index + 1:]
+
+                return list_to_return
+            statement_list = statement_to_list(statement)
+
+            # If too many arguments given (>3), then print error, and jump to the next iteration
+            if len(statement_list) > 1 + 3:
+                statement = input("Too many arguments (more than 3) were given! Enter another statement:")
+                continue
+
+
+            # Read the FIRST argument (index 1), and check that it is a legitimate encrypt/decrypt cipher. If not a
+            # legitimate cipher, then print error and get the user to enter another command.
+            cipher = statement_list[1]
+            if not ((command == "encrypt" and cipher in misc.ENCRYPTION_SET) \
+                       or (command == "decrypt" and cipher in misc.DECRYPTION_SET)):     # If not legitimate cipher
+                statement = input("Cipher (" + cipher + ") not recognized as "
+                                  + ("an" if command == "encrypt" else "a")
+                                  + " "
+                                  + command
+                                  + "ion cipher! Enter another statement: ")
+                continue
+
+
+
+            # Read the SECOND argument (source file path). Get data from source. If there is a failure, then print
+            # error and get the user to enter another command
+            input_path = statement_list[2]
+
+
+            # If the input is "last", then use the data from the file path "pointed" to by last
+            if input_path == "last":
+
+                #  open the file and store its contents in the string data
+                try:
+                    my_file = open(last, "r", encoding="utf-8")
+                    data = my_file.read()
+                    my_file.close()
+                except IOError:
+                    statement = input("There is no last file! Try again: ")
+                    continue
+
+            # Otherwise, a source is given (NOT last)
+            elif not input_path == "last":
+
+                #  Try to open the file as is (the literal file path)
+                try:
+                    my_file = open(input_path, "r", encoding="utf-8")
+                    data = my_file.read()
+                    my_file.close()
+
+                except IOError:  # Search within Resources/Library, Resources/Files_Encrypted...
+                    try:                                                              # Search Resources/Library
+                        my_file = open("Resources/Library/"
+                                       + input_path, "r", encoding="utf-8")
+                        data = my_file.read()
+                        my_file.close()
+
+                    except IOError:
+                        try:                                                          # Search Resources/Files_Encrypted
+                            my_file = open("Resources/Files_Encrypted/"
+                                           + input_path, "r", encoding="utf-8")
+                            data = my_file.read()
+                            my_file.close()
+
+                        except IOError:
+                            try:                                                      # Search Resources/Files_Decrypted
+                                my_file = open("Resources/Files_Decrypted/"
+                                               + input_path, "r", encoding="utf-8")
+                                data = my_file.read()
+                                my_file.close()
+
+                            except IOError:                                           # File not found
+                                # File could not be found, inform the user of the error
+                                statement = input("No such file or directory! Try again: ")
+                                continue
+
+                 # if the input was empty, return an error
+                if len(data) == 0:
+                    statement = input("There is no data to process in file("
+                                      + input_path + ")! Try again: ")
+                    continue
+
+
+
+
+
+
+
+
+        # The command has not been found. Tell the user to enter another legitimate command
+        else:
+
+            # Print out prompt
+            statement = input("Command (" + command + ") not recognized! Enter another statement: ")
+
+
+
 
     """
     statement = input("Enter statement: ") # obtain user input
@@ -337,12 +466,12 @@ def parse_user_input():
 
             #  make sure that the given cipher is a valid type for encryption
             if encrypt:
-                if given_cipher in misc.encryption_set:
+                if given_cipher in misc.ENCRYPTION_SET:
                         cipher = given_cipher
 
             # if in decryption mode, make sure a valid decryption cipher is chosen
             else:
-                if given_cipher in misc.decryption_set:
+                if given_cipher in misc.DECRYPTION_SET:
                         cipher = given_cipher
 
             # if there is no valid cipher, return error
