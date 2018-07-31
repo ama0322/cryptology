d26106a1bd0cb2b3adac7ed2f9a91714e655d3f4
ama0322 <ma.aaron.ma@gmail.com>
2018-07-31 04:40:08 -0700

    Type hints and list optimization.

--- Cryptography/Decryption/rotation.py
+++ Cryptography/Decryption/rotation.py
@@ -15,13 +15,13 @@ ciphertext_alphabet_restricted = True
 
 
 # Decrypt using user-entered info. Write relevant information and return decrypted text for cryptography_runner
-def execute(data, output_location):
+def execute(data:str, output_location:str) -> None:
     """
     This function decrypts data using a user-provided key.
 
-    :param data: (string) the data to be decrypted
-    :param output_location: (string) the location to write out relevant info and statistics
-    :return: (string) the decrypted data
+    :param data:            (str) the data to be decrypted
+    :param output_location: (str) the location to write out relevant info and statistics
+    :return:                None
     """
 
 
@@ -31,19 +31,20 @@ def execute(data, output_location):
 
 
 
+
 # Figure out the encryption and decryption code. Pass info to misc' testing_execute function
-def testing_execute(encryption, decryption, plaintext, plaintext_source, encryption_key, alphabet_size,
-                    output_location):
+def testing_execute(encryption:str, decryption:str, plaintext:str, plaintext_source:str, encryption_key:str,
+                    alphabet_size:int, output_location:str) -> None:
     """
     Conducts a rotation decryption in testing mode
 
-    :param encryption: (string) the name of the encryption cipher to use
-    :param decryption: (string) the name of the decryption cipher to use (this)
-    :param plaintext_source: (string) the location where the plaintext is found
-    :param plaintext: (string) the plaintext to encrypt
-    :param encryption_key: (string) the key to use to encrypt
-    :param alphabet_size: (int) the size of the character set to use
-    :param output_location: (string) the name of the file to write statistics in
+    :param encryption:       (str) the name of the encryption cipher to use
+    :param decryption:       (str) the name of the decryption cipher to use (this)
+    :param plaintext_source: (str) the location where the plaintext is found
+    :param plaintext:        (str) the plaintext to encrypt
+    :param encryption_key:   (str) the key to use to encrypt
+    :param alphabet_size:    (int) the size of the character set to use
+    :param output_location:  (str) the name of the file to write statistics in
     :return: None
     """
 
@@ -65,19 +66,18 @@ def testing_execute(encryption, decryption, plaintext, plaintext_source, encrypt
 
 
 
-
 # Returns string. This is the actual algorithm to decrypt
-def decrypt(ciphertext, key, alphabet_size):
+def decrypt(ciphertext:str, key:str, alphabet_size:int) -> str:
     """
     This function decrypts the ciphertext using the set of unicode characters from 0 to end_char.
 
-    :param ciphertext: (string )the text to be encrypted
-    :param key: (string) the key with which the encryption is done
+    :param ciphertext:    (str) the text to be encrypted
+    :param key:           (str) the key with which the encryption is done
     :param alphabet_size: (int) The number of characters in the character set
-    :return: (string) the encrypted text
+    :return:              (str) the encrypted text
     """
 
-    encrypted = "" # the string to build up the encrypted text
+    encrypted = [] # the list to build up the encrypted text
     key_index = 0 # the index in the key we are using for the vigenere encrypt
 
 
@@ -91,11 +91,21 @@ def decrypt(ciphertext, key, alphabet_size):
 
 
         #  figure out the character by subtracting the two ascii's, the add it to the encrypted string
-        encrypted_char = chr((uni_val_cipher - uni_val_key) % (alphabet_size))
-        encrypted = encrypted + encrypted_char
+        encrypted_char = chr((uni_val_cipher - uni_val_key) % alphabet_size)
+        encrypted.append(encrypted_char)
 
+
+
+    encrypted = "".join(encrypted)
 
 
     return encrypted
+
+
+
+
+
+
+
 
 
--- Cryptography/Encryption/rotation.py
+++ Cryptography/Encryption/rotation.py
@@ -9,7 +9,7 @@ from Cryptography import misc
 ########################################################################################## STANDARD FUNCTIONS ##########
 
 # Encrypt using user-entered info. Write relevant information and return encrypted text for cryptography_runner
-def execute(data, output_location):
+def execute(data:str, output_location:str) -> None:
     """
     This function calls the appropriate functions in misc.py. Those functions will use the encrypt() function
     located below as the algorithm to actually encrypt the text. Then, the ciphertext will be returned back to
@@ -17,7 +17,6 @@ def execute(data, output_location):
 
     :param data: (string) the data to be encrypted
     :param output_location: (string) the location to print out the information
-    :return: (string) the encrypted data
     """
 
     # Encrypt the plaintext. Print out the ciphertext and relevant information
@@ -28,17 +27,17 @@ def execute(data, output_location):
 
 
 # This function contains the actual algorithm to encrypt in a rotation cipher using a key
-def encrypt(plaintext, key, alphabet_size):
+def encrypt(plaintext:str, key:str, alphabet_size:int) -> str:
     """
     This function encrypts the plaintext using the set of unicode characters from 0 to alphabet_size - 1.
 
-    :param plaintext: (string )the text to be encrypted
-    :param key: (string) the key with which the encryption is done
+    :param plaintext:     (str) the text to be encrypted
+    :param key:           (str) the key with which the encryption is done
     :param alphabet_size: (int) The number of characters in the character set
-    :return: (string) the encrypted text
+    :return:              (str) the encrypted text
     """
 
-    encrypted = "" # the string to build up the encrypted text
+    encrypted = [] # the list to build up the encrypted text
     key_index = 0 # the index in the key we are using for the vigenere encrypt
 
 
@@ -52,12 +51,13 @@ def encrypt(plaintext, key, alphabet_size):
 
 
         #  figure out the character by combining the two ascii's, the add it to the encrypted string
-        encrypted_char = chr((uni_val_plain + uni_val_key) % (alphabet_size))
-        encrypted = encrypted + encrypted_char
+        encrypted_char = chr((uni_val_plain + uni_val_key) % alphabet_size)
+        encrypted.append(encrypted_char)
 
 
 
 
+    encrypted = "".join(encrypted)
 
     return encrypted
 
