2fb99e61f70f0174523096f26a5ac494c401663e
ama0322 <ma.aaron.ma@gmail.com>
2018-07-31 16:57:30 -0700

    Added type hints.

--- Cryptography/Decryption/vigenere_exponential.py
+++ Cryptography/Decryption/vigenere_exponential.py
@@ -12,13 +12,13 @@ key_size = "multiple characters"
 
 ########################################################################################## STANDARD FUNCTIONS ##########
 
-def execute(data, output_location):
+def execute(data:str, output_location:str) -> None:
     """
     This function decrypts data using a key.
 
-    :param output_location: (string) the file path to store the output in
-    :param data: (string) the data to be decrypted
-    :return: the decrypted data
+    :param output_location: (str) the file path to store the output in
+    :param data:            (str) the data to be decrypted
+    :return:                None
     """
 
     # Encrypt the plaintext. Print out the ciphertext and relevant information
@@ -30,19 +30,19 @@ def execute(data, output_location):
 
 
 # Figure out the encryption and decryption code. Pass info to misc' testing_execute function
-def testing_execute(encryption, decryption, plaintext, plaintext_source, encryption_key, alphabet_size,
-                    output_location):
+def testing_execute(encryption:str, decryption:str, plaintext:str, plaintext_source:str, encryption_key:str,
+                    alphabet_size:int, output_location:str) -> None:
     """
     Conducts a vigenere_exponential decryption in testing mode
 
-    :param encryption: (string) the name of the encryption cipher to use
-    :param decryption: (string) the name of the decryption cipher to use (this)
-    :param plaintext_source: (string) the location where the plaintext is found
-    :param plaintext: (string) the plaintext to encrypt
-    :param encryption_key: (string) the key to use to encrypt
-    :param alphabet_size: (int) the size of the character set to use
-    :param output_location: (string) the name of the file to write statistics in
-    :return: None
+    :param encryption:       (str) the name of the encryption cipher to use
+    :param decryption:       (str) the name of the decryption cipher to use (this)
+    :param plaintext_source: (str) the location where the plaintext is found
+    :param plaintext:        (str) the plaintext to encrypt
+    :param encryption_key:   (str) the key to use to encrypt
+    :param alphabet_size:    (int) the size of the character set to use
+    :param output_location:  (str) the name of the file to write statistics in
+    :return:                 None
     """
 
 
@@ -62,21 +62,24 @@ def testing_execute(encryption, decryption, plaintext, plaintext_source, encrypt
 
 
 
+
+
+
 # Returns string. This is the actual algorithm to decrypt
-def decrypt(ciphertext, key, alphabet_size):
+def decrypt(ciphertext:str, key:str, alphabet_size:int) -> str:
     """
     This function decrypts with vigenere. Instead of exponents, take the nth root.
 
-    :param ciphertext: (string) the ciphertext to decrypt
-    :param key: (string) the string to decrypt with
+    :param ciphertext:    (str) the ciphertext to decrypt
+    :param key:           (str) the string to decrypt with
     :param alphabet_size: (int) the size of the character set used
-    :return: (string) the deciphered text
+    :return:              (str) the deciphered text
     """
 
-    plaintext = ""
+    plaintext = []
     key_index = 0
     char_counter = 0
-    num_chars = ciphertext.count(" ")
+    num_chars = len(ciphertext)
 
     # Adjust the alphabet size to exclude surrogates (if necessary)
     if alphabet_size > 256:
@@ -92,7 +95,7 @@ def decrypt(ciphertext, key, alphabet_size):
         # Print updates (every 1000 characters)
         if char_counter % 100 == 0:
             print("DECRYPTION\tPercent of text done: " + str((char_counter / num_chars) * 100))
-        char_counter += 1
+        char_counter += len(ciphertext[0: ciphertext.find(" ", 1)]) + 1         # The length of one block
 
 
         # Read in one block/unit (one char, followed by a number, followed by a space). Then, update ciphertext
@@ -126,10 +129,15 @@ def decrypt(ciphertext, key, alphabet_size):
 
 
         # Add plain char to plaintext
-        plaintext += plain_char
+        plaintext.append(plain_char)
 
+
+    plaintext = "".join(plaintext)
 
     return plaintext
+
+
+
 
 
 
