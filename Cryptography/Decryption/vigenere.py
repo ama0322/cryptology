bf725cec7384f6e47383d430bbef627712651967
ama0322 <ma.aaron.ma@gmail.com>
2018-07-31 17:03:21 -0700

    Added type hints.

--- Cryptography/Decryption/vigenere.py
+++ Cryptography/Decryption/vigenere.py
@@ -16,13 +16,13 @@ ciphertext_alphabet_restricted = True
 
 
 # Call the proper functions to decrypt. Return decrypted text back to cryptography_runner.py
-def execute(data, output_location):
+def execute(data:str, output_location:str) -> None:
     """
     This function decrypts data using a key.
 
-    :param data: (string) the data to be decrypted
-    :param output_location: (string) the location to save relevant info into
-    :return: (string) the decrypted data
+    :param data:            (str) the data to be decrypted
+    :param output_location: (str) the location to save relevant info into
+    :return:                None
     """
 
 
@@ -33,19 +33,19 @@ def execute(data, output_location):
 
 
 # Figure out the encryption and decryption code. Pass info to misc' testing_execute function
-def testing_execute(encryption, decryption, plaintext, plaintext_source, encryption_key, alphabet_size,
-                    output_location):
+def testing_execute(encryption:str, decryption:str, plaintext:str, plaintext_source:str, encryption_key:str,
+                    alphabet_size:int, output_location:str) -> None:
     """
     Conducts a vigenere decryption in testing mode
 
-    :param encryption: (string) the name of the encryption cipher to use
-    :param decryption: (string) the name of the decryption cipher to use (this)
-    :param plaintext_source: (string) the location where the plaintext is found
-    :param plaintext: (string) the plaintext to encrypt
-    :param encryption_key: (string) the key to use to encrypt
-    :param alphabet_size: (int) the size of the character set to use
-    :param output_location: (string) the name of the file to write statistics in
-    :return: None
+    :param encryption:       (str) the name of the encryption cipher to use
+    :param decryption:       (str) the name of the decryption cipher to use (this)
+    :param plaintext_source: (str) the location where the plaintext is found
+    :param plaintext:        (str) the plaintext to encrypt
+    :param encryption_key:   (str) the key to use to encrypt
+    :param alphabet_size:    (int) the size of the character set to use
+    :param output_location:  (str) the name of the file to write statistics in
+    :return:                 None
     """
 
 
@@ -68,14 +68,14 @@ def testing_execute(encryption, decryption, plaintext, plaintext_source, encrypt
 
 
 # Returns string. This is the actual algorithm to decrypt
-def decrypt(ciphertext, key, alphabet_size):
+def decrypt(ciphertext:str, key:str, alphabet_size:int) -> str:
     """
     This function decrypts with vigenere. Instead of adding, subtract
 
-    :param ciphertext: (string) the ciphertext to decrypt
-    :param key: (string) the key to decrypt with
-    :param alphabet_size: (integer) the size of the character set that is used
-    :return: (string) the deciphered text
+    :param ciphertext:    (str) the ciphertext to decrypt
+    :param key:           (str) the key to decrypt with
+    :param alphabet_size: (int) the size of the character set that is used
+    :return:              (str) the deciphered text
     """
 
     # Build up the decrypted text here
