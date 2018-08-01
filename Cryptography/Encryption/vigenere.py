4b9ea0c4abdb70d32e3dea3787dfb31f0583d6ea
ama0322 <ma.aaron.ma@gmail.com>
2018-07-31 17:05:28 -0700

    Added type hints.

--- Cryptography/Encryption/vigenere.py
+++ Cryptography/Encryption/vigenere.py
@@ -9,15 +9,15 @@ from Cryptography import misc
 
 ########################################################################################## STANDARD FUNCTIONS ##########
 
-def execute(data, output_location):
+def execute(data:str, output_location:str) -> None:
     """
     This function calls the appropriate functions in misc.py. Those functions will use the encrypt() function
     located below as the algorithm to actually encrypt the text. Then, the ciphertext will be returned back to
     cryptography_runner.py
 
-    :param data: (string) the data to be encrypted
-    :param output_location: (string) the file to write relevant information into
-    :return: (string) the encrypted data
+    :param data:            (str) the data to be encrypted
+    :param output_location: (str) the file to write relevant information into
+    :return:                None
     """
 
 
@@ -29,15 +29,15 @@ def execute(data, output_location):
 
 
 # The actual algorithm to encrypt using a vigenere cipher
-def encrypt(plaintext, key, alphabet_size):
+def encrypt(plaintext:str, key:str, alphabet_size:int) -> str:
     """
     This function encrypts with a straight vigenere cipher. This uses the set of unicode values from 0 to
     alphabet_size - 1.
 
-    :param plaintext: (string) the plaintext to encrypt with
-    :param key: (string) the string to encrypt with
-    :param alphabet_size: (integer) the number of characters in the character set used
-    :return: (string) the encrypted text
+    :param plaintext:     (str) the plaintext to encrypt with
+    :param key:           (str) the string to encrypt with
+    :param alphabet_size: (int) the number of characters in the character set used
+    :return:              (str) the encrypted text
     """
 
     ciphertext = "" # The string used to build up the encrypted text, one character at a time
