d2e5ff1b63466ea10ba43962740c3c992a070b6c
ama0322 <ma.aaron.ma@gmail.com>
2018-07-31 17:13:49 -0700

    Added type hints.

--- Cryptography/Decryption/rsa.py
+++ Cryptography/Decryption/rsa.py
@@ -19,14 +19,14 @@ key_bits = 2048
 ########################################################################################## STANDARD FUNCTIONS ##########
 
 
-# Call the proper functions to decrypt. Return decrypted text bac to cryptography_runner.py
-def execute(data, output_location):
+# Call the proper functions to decrypt. Write out the decrypted text and relevant information
+def execute(data:str, output_location:str) -> None:
     """
     This function decrypts data using a key.
 
-    :param data: (string) the data to be decrypted
-    :param output_location: (string) the location to save relevant info into
-    :return: (string) the decrypted data
+    :param data:            (str) the data to be decrypted
+    :param output_location: (str) the location to save relevant info into
+    :return:                (str) the decrypted data
     """
 
     # Decrypt the ciphertext. Write the plaintext and info to a file
@@ -37,19 +37,19 @@ def execute(data, output_location):
 
 
 # Figure out the encryption and decryption code. Pass info to misc' testing_execute function
-def testing_execute(encryption, decryption, plaintext, plaintext_source, encryption_key, encoding,
-                    output_location):
+def testing_execute(encryption:str, decryption:str, plaintext:str, plaintext_source:str, encryption_key:str,
+                    encoding:str, output_location:str) -> None:
     """
     Conducts an rsa decryption in testing mode
 
-    :param encryption: (string) the name of the encryption cipher to use
-    :param decryption: (string) the name of the decryption cipher to use (this)
-    :param plaintext_source: (string) the location where the plaintext is found
-    :param plaintext: (string) the plaintext to encrypt
-    :param encryption_key: (string) the key to use to encrypt
-    :param encoding: (str) the size of the character set to use
-    :param output_location: (string) the name of the file to write statistics in
-    :return: None
+    :param encryption:       (str) the name of the encryption cipher to use
+    :param decryption:       (str) the name of the decryption cipher to use (this)
+    :param plaintext_source: (str) the location where the plaintext is found
+    :param plaintext:        (str) the plaintext to encrypt
+    :param encryption_key:   (str) the key to use to encrypt
+    :param encoding:         (str) the size of the character set to use
+    :param output_location:  (str) the name of the file to write statistics in
+    :return:                 None
     """
 
     # Store statistics from the last encryption here(Just declarations):
@@ -117,14 +117,14 @@ def testing_execute(encryption, decryption, plaintext, plaintext_source, encrypt
 
 
 # Returns string. This is the actual algorithm to decrypt
-def decrypt(ciphertext, private_key, encoding_scheme):
+def decrypt(ciphertext:str, private_key:str, encoding_scheme:str) -> str:
     """
     This function decrypts with rsa cipher
 
-    :param ciphertext: (string) the ciphertext to decrypt
-    :param private_key: (string) the key to decrypt with. In format "(encoding scheme) d = ..., n = ..."
-    :param encoding_scheme: (string) the type of encoding scheme that is used
-    :return: (string) the deciphered text
+    :param ciphertext:      (str) the ciphertext to decrypt
+    :param private_key:     (str) the key to decrypt with. In format "(encoding scheme) d = ..., n = ..."
+    :param encoding_scheme: (str) the type of encoding scheme that is used
+    :return:                (str) the deciphered text
     """
 
     plaintext = "" # Build up the decrypted text here
@@ -188,13 +188,13 @@ def decrypt(ciphertext, private_key, encoding_scheme):
 
 
 # Reads an rsa key and returns the public/private key and modulus
-def read_rsa_key(key):
+def read_rsa_key(key:str) -> (int, int):
     """
     This reads the rsa key which is in the format of "RSA (character length of e or d) (e or d) n"
 
-    :param key: (string) the rsa key
-    :return: (int) the public/private exponent key
-    :return: (int) the modulus for rsa
+    :param key: (str) the rsa key
+    :return:    (int) the public/private exponent key
+    :return:    (int) the modulus for rsa
     """
 
     # Figure out the character scheme of the key
